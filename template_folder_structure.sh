427fd0151fa3b1f97477481156e9407f34c568df
mariahesselman <maria.carmen.hesselman@stud.ki.se>
2018-02-16 12:01:21 +0100

    Add bash script for folder structure

--- template_folder_structure.sh
+++ template_folder_structure.sh
@@ -1,1 +1,13 @@
-f
+mkdir Buried_exposed_alpha_helical_proteins
+cd Buried_exposed_alpha_helical_proteins
+mkdir ./scripts/
+mkdir ./bash/
+touch ./bash/runall.sh
+touch ./bash/filter*sh
+mkdir ./input/
+mkdir ./logs
+touch readme.txt
+touch commands.txt
+mkdir ./datasets/
+mkdir ./general_scripts/
+mkdir ./bin/
