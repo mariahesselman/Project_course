d9d801e630d6b607198f61ff287a3dcb738022a3
mariahesselman <maria.carmen.hesselman@stud.ki.se>
2018-02-19 10:41:02 +0100

    Change ending on filter file in bash script

--- template_folder_structure.sh
+++ template_folder_structure.sh
@@ -3,7 +3,7 @@ cd Buried_exposed_alpha_helical_proteins
 mkdir ./scripts/
 mkdir ./bash/
 touch ./bash/runall.sh
-touch ./bash/filter*sh
+touch ./bash/filter.sh
 mkdir ./input/
 mkdir ./logs
 touch readme.txt
