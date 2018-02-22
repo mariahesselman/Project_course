24b03849825d9c19bc34df880067a19777d1e767
mariahesselman <maria.carmen.hesselman@stud.ki.se>
2018-02-22 17:02:09 +0100

    started one hot encoder after parser

--- Buried_exposed_alpha_helical_proteins/codes/parser.py
+++ Buried_exposed_alpha_helical_proteins/codes/parser.py
@@ -1,10 +1,12 @@
 import pandas as pd
-def parser(testfile):
+import numpy as np
+from numpy import argmax
+def parser(filename):
     dic = {}
     keys = []
     seq = []
     topo = []
-    file = open(testfile, 'r')
+    file = open(filename, 'r')
     with file as f:
         for linenumber, line in enumerate(f):            
             if line[0] == '>':
@@ -21,9 +23,25 @@ def parser(testfile):
     #print(topo)
     #print(dic) 
     df = pd.DataFrame(data=dic)
-    print(df)
+    #print(df)
+    sequences = df.loc[0]
+    #print(sequences)
     file.close()
-parser('dataset')
+    return sequences
+def enconde(filename):
+#One-hot ecnoding aa alphabet
+    
+    seq = parser(filename)
+    print(seq)
+    aa = 'GALMFWKQESPVICYHRNDT'
+    aa_to_int = dict((c, i) for i, c in enumerate(aa))
+    int_to_aa = dict((i, c) for i, c in enumerate(aa))
+    print(aa_to_int)
+    #integer_encoded = [aa_to_int[char] for char in seq]
+    #print(integer_encoded)
+
+
         
-            
-            
+if __name__ == '__main__':
+    parser('test.txt')            
+    enconde('test.txt')        
