6afd3a072786e24f53e4fb0763bba70a325c37d9
mariahesselman <maria.carmen.hesselman@stud.ki.se>
2018-02-23 17:45:47 +0100

    add integer encoding of seqs and input window size

--- Buried_exposed_alpha_helical_proteins/codes/parser.py
+++ Buried_exposed_alpha_helical_proteins/codes/parser.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
 from numpy import argmax
-def parser(filename):
-    dic = {}
-    keys = []
-    seq = []
-    topo = []
+dic = {}
+keys = []
+seq = []
+topo = []
+
+def parser(filename):    
     file = open(filename, 'r')
     with file as f:
         for linenumber, line in enumerate(f):            
@@ -16,32 +17,25 @@ def parser(filename):
             elif linenumber%3 == 2:
                 topo.append(line[:-1])
         for i in range(len(keys)):
-            dic[(keys[i])] = (seq[i], topo[i])            
-                               
-    #print(keys)
-    #print(seq)
-    #print(topo)
-    #print(dic) 
-    df = pd.DataFrame(data=dic)
-    #print(df)
-    sequences = df.loc[0]
-    #print(sequences)
-    file.close()
-    return sequences
-def enconde(filename):
-#One-hot ecnoding aa alphabet
-    
-    seq = parser(filename)
+            dic[(keys[i])] = (seq[i], topo[i])
+    print(dic)                                               
+    file.close()    
+#One-hot ecnoding aa alphabet    
     print(seq)
     aa = 'GALMFWKQESPVICYHRNDT'
     aa_to_int = dict((c, i) for i, c in enumerate(aa))
-    int_to_aa = dict((i, c) for i, c in enumerate(aa))
     print(aa_to_int)
-    #integer_encoded = [aa_to_int[char] for char in seq]
-    #print(integer_encoded)
-
+#Defining window size
+    window = int(input('Input window size (must be odd number): '))
+#Integer-encoding protein sequences from list(seq)    
+    integer_encoded_seq = []
+    for item in range(len(seq)):
+        integer_encoded = [aa_to_int[char] for char in seq[item]]
+        integer_encoded_seq.append(integer_encoded)
+    print(integer_encoded_seq)
+    
 
         
 if __name__ == '__main__':
     parser('test.txt')            
-    enconde('test.txt')        
+       
