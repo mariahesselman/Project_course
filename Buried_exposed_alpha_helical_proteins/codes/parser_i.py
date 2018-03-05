3abc781932c66933b957868210fb216b8b310ef1
mariahesselman <maria.carmen.hesselman@stud.ki.se>
2018-03-05 17:45:16 +0100

    parser that trains and saves model

--- Buried_exposed_alpha_helical_proteins/codes/parser_i.py
+++ Buried_exposed_alpha_helical_proteins/codes/parser_i.py
@@ -1,27 +1,34 @@
 import numpy as np
+from sklearn import svm
+from sklearn.externals import joblib
+
 aa = '0GALMFWKQESPVICYHRNDT'
-aa_to_int = dict((c, i) for i, c in enumerate(aa))
+AA_TO_INT = dict((c, i) for i, c in enumerate(aa))
+
 
 def parse_name(dataset):
     input_file = open(dataset, 'r')
     input_list = [(line.strip()) for line in input_file]
     name = [element for element in input_list[0::3]]       
     input_file.close()
-    return(name)
+    return name
+
 
 def parse_seq(dataset):
     input_file = open(dataset, 'r')
     input_list = [(line.strip()) for line in input_file]
     seq = [element for element in input_list[1::3]]
     input_file.close()
-    return(seq)
+    return seq
+
 
 def parse_topo(dataset):
     input_file = open(dataset, 'r')
     input_list = [(line.strip()) for line in input_file]    
     topo = [element for element in input_list[2::3]]
     input_file.close()
-    return(topo)
+    return topo
+
 
 def windowmaking(dataset, window):
     padding = window//2
@@ -29,25 +36,29 @@ def windowmaking(dataset, window):
     seq = parse_seq(dataset)
     
     for sequence in seq:
-        sequence = ((padding)*'0' + sequence + ((padding)*'0'))
+        sequence = ((padding*'0') + sequence + (padding*'0'))
         for residue in range(0, len(sequence)):
             if residue+(window) > len(sequence):
                 break
             window_list.append(sequence[residue:residue+window])
-    return(window_list)    
+    return window_list    
 
-######Making the aminoacids in the windows to integers
+# Making the aminoacids in the windows to integers
+
+
 def window_int(dataset, window): 
     window_list = windowmaking(dataset, window)
     windows_in_integers = []    
     
     for window in window_list:
-        windows_in_int = [aa_to_int[aa] for aa in window]    
+        windows_in_int = [AA_TO_INT[aa] for aa in window]    
         windows_in_integers.append(windows_in_int)
-    return(windows_in_integers)
+    return windows_in_integers
 
 
-######One hot encoding windows
+# One hot encoding windows
+
+
 def onehot(dataset, window): 
     windows = window_int(dataset, window)
     onehot_encoded_windows = []
@@ -63,7 +74,7 @@ def onehot(dataset, window):
         onehot_encoded_windows.append(this_window)
     
     burial = {'E':1, 'B':-1}
-    burial_decode = {1:'E', -1:'B'}
+
     topology_list = []
     topo = parse_topo(dataset)
     for proteins in topo:    
@@ -71,17 +82,15 @@ def onehot(dataset, window):
             y = burial[topologies]
             topology_list.append(y)   
     outfile = 'SVM_input'
-    np.savez(outfile, x=onehot_encoded_windows, y=topology_list) #saves a file of my x and y
+    np.savez(outfile, x=onehot_encoded_windows, y=topology_list)
     return(onehot_encoded_windows, topology_list)
 
-#####Training
-from sklearn import svm
-from sklearn.externals import joblib
+# Training
+
+
 def svmm():
     np.set_printoptions(threshold=np.inf)
     filz = np.load('SVM_input.npz')
-    #X = onehot_encoded_windows
-    #Y = topology_list
     x = filz['x']
     y = filz['y']
     clf = svm.SVC()
@@ -90,19 +99,7 @@ def svmm():
     filename = 'model.sav'
     joblib.dump(clf, filename)
 
+
 if __name__ == '__main__':    
-    onehot('dataset', 25)
-    #print(onehot('test.txt', 3))    
-    #print(enc_burial('test.txt'))    
-    svmm()   
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    onehot('dataset', 25)    
+    svmm()         
