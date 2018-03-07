faa47eb6f9a63fbc8de13d7b21e0ae639a53332b
mariahesselman <maria.carmen.hesselman@stud.ki.se>
2018-03-07 17:57:31 +0100

    predicting sequence by sequence

--- Buried_exposed_alpha_helical_proteins/codes/predict.py
+++ Buried_exposed_alpha_helical_proteins/codes/predict.py
@@ -5,7 +5,7 @@ import numpy as np
 loaded_model = joblib.load('model.sav')
 aa = '0GALMFWKQESPVICYHRNDT'
 AA_TO_INT = dict((c, i) for i, c in enumerate(aa))
-
+dic = {}
 def parse_fasta(filename):
     input_file = open(filename, 'r')
     id_list = []
@@ -17,54 +17,71 @@ def parse_fasta(filename):
             seq_list.append(line.strip())
     print(id_list)
     print(seq_list)
-    return seq_list
+    for i in range(len(id_list)):
+        dic[(id_list[i])] = (seq_list[i], )    
+    return dic
 
 
 def windowmaking(seq, window):
     padding = window//2
+    padded_sequences = []
     window_list = []    
-    for sequence in seq:
-        sequence = ((padding*'0') + sequence + (padding*'0'))
-        for residue in range(0, len(sequence)):
-            if residue+(window) > len(sequence):
+    for id_, sequence in dic.items():
+        sequence = sequence[0]
+        padded_sequences.append((padding*'0') + sequence + (padding*'0'))
+        #print(sequence)
+    for padded_seq in padded_sequences:
+        this_wind = []
+        for residue in range(len(padded_seq)):
+            if residue+(window) > len(padded_seq):
                 break
-            window_list.append(sequence[residue:residue+window])
-        return window_list
-
+            this_wind.append(padded_seq[residue:residue+window])
+        window_list.append(this_wind)
+    #print(padded_sequences)
+    #print(window_list)
+    return window_list
+# window_list contains a list of sequences divided into sublists of sliding windows.
 
 def window_int(windows): 
     windows_in_integers = []
-    for window in windows:
-        windows_in_int = [AA_TO_INT[aa] for aa in window]    
-        windows_in_integers.append(windows_in_int)
+    for sequence in windows:
+        temp = []
+        for window in sequence:
+            windows_in_int = [AA_TO_INT[aa] for aa in window]    
+            temp.append(windows_in_int)
+        windows_in_integers.append(temp)
+    #print(windows_in_integers)
     return windows_in_integers    
 
 
 def onehot(int_wind): 
     onehot_encoded_windows = []
-    for window in int_wind:
-        this_window = []
-        for residue in window:
-            letter = [0 for _ in range(len(aa))]
-            if residue != 0:      
-                letter[residue] = 1
-            else:
-                pass
-            this_window.extend(letter)           
-        onehot_encoded_windows.append(this_window)
+    for sequence in int_wind:        
+        for window in sequence:
+            this_window = []
+            for residue in window:
+                letter = [0 for _ in range(len(aa))]
+                if residue != 0:      
+                    letter[residue] = 1
+                else:
+                    pass
+                this_window.append(letter)           
+            onehot_encoded_windows.append(this_window)
+    print(len(onehot_encoded_windows))
     return onehot_encoded_windows    
 
 
 def predict(sequence_in_windows, seqsy):
-    result = loaded_model.predict(sequence_in_windows)
-    results = result
-    burial_decode = {1:'E', -1:'B'}
-    decoded_results = []
-    for element in results:
-        decoded_results.append(burial_decode[element])
-    print(results)
-    print(seqsy, decoded_results)
-    print(len(decoded_results))
+    for sequence in sequence_in_windows:
+        result = loaded_model.predict(sequence)
+        #print(result)
+        burial_decode = {1:'E', -1:'B'}
+        decoded_results = []
+        for element in result:
+            decoded_results.append(burial_decode[element])
+    #print(results)
+    #print(seqsy, decoded_results)
+    #print(len(decoded_results))
     return decoded_results    
 if __name__ == '__main__':
     seqsy = parse_fasta(input("Please write the name of the fasta file you want to predict: "))
