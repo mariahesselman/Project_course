ae9660d23d9ff8c70731e8d41789955b9b65d41f
mariahesselman <maria.carmen.hesselman@stud.ki.se>
2018-03-14 17:55:39 +0100

    better pylint score

--- Buried_exposed_alpha_helical_proteins/PSIBLAST/scripts/predict_pssm.py
+++ Buried_exposed_alpha_helical_proteins/PSIBLAST/scripts/predict_pssm.py
@@ -1,46 +1,44 @@
-import sys
-sys.path.insert(0,"../../codes")
-import numpy as np
-from sklearn import svm
-from sklearn.model_selection import train_test_split
+"""Predicts topology from a position specific frequency table"""
 import pickle
-import parser_i
-import parse_pssm
+import sys
+import numpy as np
 
-loaded_model = pickle.load(open('../scripts/pssm_model.sav', 'rb'))
+
+LOADED_MODEL = pickle.load(open('../scripts/pssm_model.sav', 'rb'))
 
 
 def predict(pssm_file):
+    """Takes a PSSM file and predicts the topology"""
     window = 3
-    padding = window // 2    
-    pssm = np.genfromtxt(pssm_file, skip_header = 3, skip_footer = 5, usecols = range(22,42))
+    padding = window // 2
+    pssm = np.genfromtxt(pssm_file, skip_header=3, skip_footer=5, usecols=range(22, 42))
     length = len(pssm)
     predicting_array = np.zeros((length, window, 20))
     decimal_pssm = pssm / 100
     pad_matrix = np.vstack([np.zeros((padding, 20)), decimal_pssm, np.zeros((padding, 20))])
-    arrays = []    
-    
+    arrays = []
+
     for aa in range(length):
         predicting_array[aa] = pad_matrix[aa:aa + window]
     arrays.append(predicting_array.reshape(length, window *20))
     arrays = np.vstack(arrays)
-    results = loaded_model.predict(arrays)
-
-    
+    results = LOADED_MODEL.predict(arrays)
     burial_decode = {1:'E', -1:'B'}
     results_decode = []
+
     for topology in results:
         results_decode.append(burial_decode[topology])
     string_result = ''.join(results_decode)
-    
+
     with open('../pssm_svm_predictions.txt', 'a') as f:
         f.write(pssm_file[:-11] + '\n')
         f.write(string_result + '\n')
         f.close()
-    print(pssm_file[:-11])    
+    print(pssm_file[:-11])
     print(string_result)
-    
+
+
 if __name__ == '__main__':
-    pssm_file = sys.argv[1]
-    predict(pssm_file)
-    #predict('../pssm/>d1du0b_.a.4.1.1.fasta.pssm')
+    #PSSM_FILE = sys.argv[1]
+    #predict(PSSM_FILE)
+    predict('../pssm/>d1du0b_.a.4.1.1.fasta.pssm')
