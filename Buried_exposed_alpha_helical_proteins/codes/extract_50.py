78ffcca248212ec46a3ea9ec44d7db017701984f
mariahesselman <maria.carmen.hesselman@stud.ki.se>
2018-03-15 16:01:45 +0100

    extracts 50 hidden proteins

--- Buried_exposed_alpha_helical_proteins/codes/extract_50.py
+++ Buried_exposed_alpha_helical_proteins/codes/extract_50.py
@@ -1,7 +1,7 @@
 import parser_i
 
-milda = 'hidden_set.3line.txt'
-mysequences = 'dataset'
+milda = '../datasets/hidden_set.3line.txt'
+mysequences = '../datasets/dataset'
 
 mynames = parser_i.parse_name(mysequences)
 myseqs = parser_i.parse_seq(mysequences)
@@ -12,20 +12,20 @@ mildastop = parser_i.parse_topo(milda)
 mildasdic = {mildasnames[element]:(mildasseqs[element], mildastop[element]) for element in range(len(mildasnames))}
 #print(mildasdic)
 
-newseqs = open('hidden.fasta', 'w')
+newseqs = open('../datasets/hidden.fasta', 'w')
 for element in range(len(mildasnames)):
     newseqs.write(mildasnames[element] + '\n')
     newseqs.write(mildasseqs[element] + '\n')
     
-myy = open('myseqs', 'w')
+myy = open('../datasets/myseqs', 'w')
 for element in range(len(mynames)):
     myy.write(mynames[element] + '\n')
     myy.write(myseqs[element] + '\n')
     
-ids = open('blinddataset_id.txt', 'r')
+ids = open('../datasets/blinddataset_id.txt', 'r')
 lines = ids.readlines()
 
-f = open('50hidden.3line.txt', 'r+')
+f = open('../datasets/50hidden.3line.txt', 'r+')
 
 for line in lines:
     line = '>' + line.strip()
@@ -33,7 +33,7 @@ for line in lines:
         f.write(line + '\n')
         f.write(mildasdic[line][0] + '\n')
         f.write(mildasdic[line][1] + '\n')
-fi = '50hidden.3line.txt'
+fi = '../datasets/50hidden.3line.txt'
 hiddennames = parser_i.parse_name(fi)
 hiddenseqs = parser_i.parse_seq(fi)
 # making sure that the hidden 50 are not in my trainin dataset.
@@ -41,7 +41,7 @@ for element in range(len(mynames)):
     if mynames[element] == hiddennames[element]:
         print(warning)
 
-fasta = open('50hidden.fasta', 'w')
+fasta = open('../datasets/50hidden.fasta', 'w')
 for element in range(50):
     fasta.write(hiddennames[element] + '\n')
     fasta.write(hiddenseqs[element] + '\n')
