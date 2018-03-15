feb0a5b67695258ec74cc49fa77a184ba5c015ee
mariahesselman <maria.carmen.hesselman@stud.ki.se>
2018-03-15 16:01:02 +0100

    calculate accuracy of pred 50 hidden

--- Buried_exposed_alpha_helical_proteins/codes/accuracy50hidden.py
+++ Buried_exposed_alpha_helical_proteins/codes/accuracy50hidden.py
@@ -1,3 +1,4 @@
+"""Writing prediction scores of hidden set to file"""
 import numpy as np
 from sklearn.metrics import matthews_corrcoef
 from sklearn.metrics import confusion_matrix
@@ -8,14 +9,14 @@ import parser_i
 
 predictedtopo =[]
 burial = {'E':1, 'B':-1}
-topo = parser_i.parse_topo('pred_50hidden.3line.txt')
+topo = parser_i.parse_topo('../result_files/predictions/opt_pred_50hidden.3line.txt')
 for proteins in topo:    
     for topologies in proteins:
         y = burial[topologies]
         predictedtopo.append(y)
 
 realtopo = []
-topo = parser_i.parse_topo('50hidden.3line.txt')
+topo = parser_i.parse_topo('../datasets/50hidden.3line.txt')
 for proteins in topo:
     prot = []    
     for topologies in proteins:
@@ -27,7 +28,7 @@ svm_classreport = classification_report(realtopo, predictedtopo, labels = [1, -1
 svm_confusionm = confusion_matrix(realtopo, predictedtopo, labels = [1, -1])
 random_mcc = matthews_corrcoef(realtopo, predictedtopo)
 
-report = open('accuracyreport50.txt', 'w')
+report = open('../result_files/opt_accuracyreport50.txt', 'w')
 report.write('Classification report:' + '\n' + str(svm_classreport) + '\n')
 report.write('Confusion matrix:' + '\n' + str(svm_confusionm) + '\n')
 report.write('MCC:' + str(random_mcc))
